98bde7e04a2db33285f0fb09d1e671a21a4ad0f2
kesaitou <ksaito1mini@gmail.com>
2025-10-12 04:01:03 +0900

    libft

--- libft/srcs/ft_bzero.c
+++ libft/srcs/ft_bzero.c
@@ -4,3 +4,15 @@ void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
 }
+
+#include <string.h>
+int main(void)
+{
+	int a = 42;
+	int b = 43;
+	int c = 44;
+	
+	
+	
+	
+}
--- libft/srcs/ft_strlen.c
+++ libft/srcs/ft_strlen.c
@@ -1,8 +1,8 @@
 #include "../includes/libft.h"
 
-size_t	ft_strlen(const char *s)
+size_t ft_strlen(const char *s)
 {
-	size_t	len;
+	size_t len;
 
 	len = 0;
 	while (s[len])
